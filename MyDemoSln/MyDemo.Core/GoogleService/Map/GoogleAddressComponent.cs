4e02eea3d5c1a8e9f732ced0706215c1723cf077
JackNi <jackni@gmail.com>
2016-02-17 21:42:37 +1030

    add missing file for google distance Matrix implementation.

---
+++ MyDemoSln/MyDemo.Core/GoogleService/Map/GoogleAddressComponent.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDemo.Core
+{
+    public class AddressComponent
+    {
+        public string long_name { get; set; }
+        public string short_name { get; set; }
+        public List<string> types { get; set; }
+    }
+
+    public class Location
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+    public class Northeast
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+    public class Southwest
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+    public class Viewport
+    {
+        public Northeast northeast { get; set; }
+        public Southwest southwest { get; set; }
+    }
+
+    public class Geometry
+    {
+        public Location location { get; set; }
+        public string location_type { get; set; }
+        public Viewport viewport { get; set; }
+    }
+
+    public class GoogleAddressResult
+    {
+        public List<AddressComponent> address_components { get; set; }
+        public string formatted_address { get; set; }
+        public Geometry geometry { get; set; }
+        public List<string> types { get; set; }
+    }
+
+    /// <summary>
+    /// This class will be the return result from google GeoCoding
+    /// </summary>
+    public class GoogleAddressReturnResult
+    {
+        public List<GoogleAddressResult> results { get; set; }
+        public string status { get; set; }
+    }
+
+    /// <summary>
+    /// this class will be used to store client actual address including google geo data.
+    /// </summary>
+    public class SystemGeoLocationResult
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+        public string GoogleReturnAddress { get; set; }
+    }
+
+    public enum GoogleGeoCodingStatus
+    {
+        OK = 1
+            ,ZERO_RESULTS = 2
+            ,OVER_QUERY_LIMIT =3 
+            ,REQUEST_DENIED = 4
+            ,INVALID_REQUEST = 5
+            ,UNKNOWN_ERROR = 6
+    }
+}
+
---
+++ MyDemoSln/MyDemo.Core/GoogleService/Map/GoogleDistanceMatrixComponent.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDemo.Core
+{
+    public class Distance
+    {
+        public string Text { get; set; }
+        public int Value { get; set; }
+    }
+
+    public class Duration
+    {
+        public string Text { get; set; }
+        public int Value { get; set; }
+    }
+
+    public class GoogleDistanceMatrixElement
+    {
+        public Distance Distance { get; set; }
+        public Duration Duration { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class GoogleDistanceMatrixRow
+    {
+        public List<GoogleDistanceMatrixElement> Elements { get; set; }
+    }
+
+    public class GoogleDistanceMatrixResult
+    {
+        public List<string> Destination_Address { get; set; }
+        public List<string> Origin_Address { get; set; }
+        public List<GoogleDistanceMatrixRow> Rows { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class GoogleDistanceQuery
+    {
+        public double OriginLat { get; set; }
+        public double OriginLng { get; set; }
+        public string DestAddress {get;set;}
+    }
+
+    public class GoogleDistanceQuery1
+    {
+        public string OriginAddress { get; set; }
+        public string DestAddress {get;set;}
+    }
+
+    public class GoogleDistanceResult
+    {
+        public string Distance { get; set; }
+        public string Duration { get; set; }
+    }
+}
+
